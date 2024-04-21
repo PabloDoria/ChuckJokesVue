76faf6bf1f3efc5d27c34422dc613b076e2761cc
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2024-04-21 12:09:16 -0600

    Primer commit

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # chuckjokesvue
 
-## Project setup
+Este proyecto es una aplicación web simple desarrollada con Vue.js que muestra una lista de chistes de Chuck Norris. Utiliza la directiva `v-for` para iterar sobre un arreglo de chistes y mostrarlos en la interfaz de usuario.
+
+## Configuración del proyecto
+
+Para configurar el proyecto, primero asegúrate de tener Node.js instalado en tu sistema. Luego, ejecuta el siguiente comando en la terminal para instalar las dependencias del proyecto:
+
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+## Compilación y recarga en caliente para desarrollo
+
+Una vez que hayas instalado las dependencias, puedes compilar y recargar en caliente la aplicación para el desarrollo ejecutando el siguiente comando:
+
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Este comando inicia un servidor de desarrollo y abre la aplicación en tu navegador web predeterminado. Cualquier cambio que realices en los archivos se reflejará automáticamente en la aplicación sin necesidad de recargar la página.
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Chistes de Chuck Norris</h1>
+    <ul>
+      <li v-for="(joke, index) in chuck" :key="index" class="joke">
+        {{ joke.value }}
+      </li>
+    </ul>x  x 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      chuck: [
+        {
+          "value": "Chuck Norris can skydive into outer space." 
+        }, 
+        { 
+          "value": "The chief export of Chuck Norris is pain." 
+        }, 
+        { 
+          "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+        }, 
+        { 
+          "value": "Time waits for no man. Unless that man is Chuck Norris." 
+        }, 
+        { 
+          "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+.joke {
+  margin: 10px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  list-style: none;
 }
 </style>
