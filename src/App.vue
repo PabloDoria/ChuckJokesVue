df4d0e47b3be8e18f5b4e556c55fca5cfd13605e
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2024-04-21 13:36:02 -0600

    Tarea 10

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,44 @@
 <template>
   <div id="app">
-    <h1>Chistes de Chuck Norris</h1>
-    <ul>
-      <li v-for="(joke, index) in chuck" :key="index" class="joke">
-        {{ joke.value }}
-      </li>
-    </ul>x  x 
+    <div class="container">
+      <h1 class="text-center my-4 display-4 title">Chistes de Chuck Norris</h1>
+      <div class="row">
+        <chuck-card v-for="(joke, index) in chuck" :key="index" :icon-url="joke.icon_url" :value="joke.value"></chuck-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ChuckCard from './components/ChuckCard.vue';
+
 export default {
   name: 'App',
+  components: {
+    ChuckCard
+  },
   data() {
     return {
       chuck: [
         {
-          "value": "Chuck Norris can skydive into outer space." 
+          "icon_url": "https://img.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2011/09/27/Style/Images/c92715.jpg?uuid=XjXZeukVEeCWYL6E-yTJeQ",
+          "value": "Chuck Norris can skydive into outer space."
         }, 
         { 
-          "value": "The chief export of Chuck Norris is pain." 
+          "icon_url": "https://th.bing.com/th/id/R.79ab9413a5faa0f5a0b1c9673ad6128d?rik=e2HQb7lwgqWWUg&riu=http%3a%2f%2fwww.thefamouspeople.com%2fprofiles%2fimages%2fchuck-norris-3.jpg&ehk=J2gLDaD5PFNFgiwioNk4VVyUz9PTtSvh88fp8JqVoZ0%3d&risl=&pid=ImgRaw&r=0",
+          "value": "The chief export of Chuck Norris is pain."
         }, 
         { 
-          "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." 
+          "icon_url": "https://sf2.closermag.fr/wp-content/uploads/closermag/2023/04/chuck-norris-los-angeles-2002-276x410.jpg",
+          "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
         }, 
         { 
-          "value": "Time waits for no man. Unless that man is Chuck Norris." 
+          "icon_url": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-150327735-copy.jpg",
+          "value": "Time waits for no man. Unless that man is Chuck Norris."
         }, 
         { 
-          "value": "If you spell Chuck Norris in Scrabble, you win. Forever." 
+          "icon_url": "https://th.bing.com/th/id/OIP.EcPCdaf4uZU-HWuLHFlkfwHaFP?w=700&h=496&rs=1&pid=ImgDetMain",
+          "value": "If you spell Chuck Norris in Scrabble, you win. Forever."
         }
       ]
     }
@@ -38,16 +48,16 @@ export default {
 
 <style>
 #app {
-  font-family: Arial, sans-serif;
-  text-align: center;
-  margin-top: 50px;
+  background-color: #f8f9fa;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.joke {
-  margin: 10px;
-  padding: 10px;
-  background-color: #f0f0f0;
-  border-radius: 5px;
-  list-style: none;
+.title {
+  color: #333;
+  font-family: 'Arial', sans-serif;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 }
 </style>
---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="col-md-4 mb-4">
+    <div class="card h-100 border-dark shadow">
+      <img :src="iconUrl" class="card-img-top chuck-img" alt="Chuck Norris">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <p class="card-text text-center">{{ value }}</p>
+        <button 
+          class="btn btn-danger btn-sm mt-2 align-self-end" 
+          :class="{ 'btn-success': liked }" 
+          @click="toggleLike"
+        >
+          <i class="fas fa-heart mr-1"></i> {{ liked ? 'Me gusta' : '¿Te gusta?' }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChuckCard',
+  props: {
+    iconUrl: String,
+    value: String
+  },
+  data() {
+    return {
+      liked: false
+    };
+  },
+  methods: {
+    toggleLike() {
+      this.liked = !this.liked;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chuck-img {
+  border-bottom: 2px solid #dc3545;
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
+}
+
+.card {
+  transition: transform 0.3s ease-in-out;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+}
+</style>
